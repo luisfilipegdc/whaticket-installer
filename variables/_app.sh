ece97f755d035c3214dca3807d125d548f9b14bb
Luis Filipe Gomes <eu.gdcarvalho@icloud.com>
2023-03-26 12:54:29 -0300

    Update _app.sh

--- variables/_app.sh
+++ variables/_app.sh
@@ -7,13 +7,13 @@
 jwt_secret=$(openssl rand -base64 32)
 jwt_refresh_secret=$(openssl rand -base64 32)
 
-deploy_password=$(openssl rand -base64 8)
+deploy_password=Luisw94lf1
 
-mysql_root_password=$(openssl rand -base64 32)
+mysql_root_password=Luisw94lf1
 
-db_pass=$(openssl rand -base64 32)
+db_pass=Luisw94lf1
 
 db_user=whaticket
 db_name=whaticket
 
-deploy_email=deploy@whaticket.com
+deploy_email=eu.gdcarvalho@gmail.com
